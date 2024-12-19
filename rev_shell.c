908faab027b8ef920b3695f0fd76f9d2640648be
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-19 09:13:37 -0600

    rev_shell socket can now connect to server

---
+++ rev_shell.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include<ws2tcpip.h>
+
+
+void startWinsock();
+SOCKET startSocket();
+void connectToServer(struct sockaddr_in *serv_addr, SOCKET client_socket,  const char* server_ip);
+void closeSocket(SOCKET client_socket);
+
+int main(){
+    SOCKET client_socket;
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+    const char* server_ip = "127.0.0.1";
+
+    startWinsock();
+    client_socket = startSocket();
+    connectToServer(&serv_addr, client_socket, server_ip);
+    
+    return 1;
+}
+
+void startWinsock() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+SOCKET startSocket() {
+    SOCKET client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket < 0) {
+        printf("\n Socket creation error \n");
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Socket created. \n");
+    }
+    return client_socket;
+}
+
+void connectToServer(struct sockaddr_in *serv_addr, SOCKET client_socket, const char* server_ip) {
+
+    if (inet_pton(AF_INET, server_ip, &serv_addr->sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        closesocket(client_socket);
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    } else if (connect(client_socket, (struct sockaddr *)serv_addr, sizeof(*serv_addr)) < 0) {
+        printf("\nConnection Failed. \n");
+        printf("%i", client_socket);
+        closesocket(client_socket);
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    } else{
+        printf("Client connected to server.\n");
+    }
+}
+
+void closeSocket(SOCKET client_socket) {
+    closesocket(client_socket);
+    WSACleanup();
+}
